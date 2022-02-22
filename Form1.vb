0fda7ddff32e4981fcd6c362dc70d75ba9840236
Ahyan Ramadhan <jbliz7665@gmail.com>
2022-02-22 10:20:32 +0700

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,32 @@
+﻿Imports System.IO
+Imports System.Net
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Create a web request that will be used to talk with the server and set the request method to upload a file by ftp.
+        Dim ftpRequest As FtpWebRequest = CType(WebRequest.Create("ftp://johnny.heliohost.org/public_html/jurnal.db"), FtpWebRequest)
+
+        Try
+            ftpRequest.Method = WebRequestMethods.Ftp.UploadFile
+
+            ' Confirm the Network credentials based on the user name and password passed in.
+            ftpRequest.Credentials = New NetworkCredential("jbliz766", "rootfs")
+
+            ' Read into a Byte array the contents of the file to be uploaded 
+            Dim bytes() As Byte = System.IO.File.ReadAllBytes("c:\jurnal.db")
+
+            ' Transfer the byte array contents into the request stream, write and then close when done.
+            ftpRequest.ContentLength = bytes.Length
+            Using UploadStream As Stream = ftpRequest.GetRequestStream()
+                UploadStream.Write(bytes, 0, bytes.Length)
+                UploadStream.Close()
+            End Using
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            Exit Sub
+        End Try
+
+        MessageBox.Show("Upload SQLite database is completed!")
+
+    End Sub
+End Class
